71908542f9ef2cdb4e098ef5adc017caaafdd1a4
Mohitku-Panigrahi <mohitkumarpanigrahi533@gmail.com>
2026-09-08 10:23:50 +0530

    Codes

---
+++ Apna/condi.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cin>>a>>b>>c;
+    // if(a>b){
+    //     cout<<a<<" is greater "<<endl;
+    // }else{
+    //     cout<<b<<" is greater"<<endl;
+    // }
+    // if(a%2==0){cout<<"Even"<<endl;}
+    // else{cout<<"Odd"<<endl;}
+
+    // //largest of three number 
+    // if(a>= b && a>=c){cout<<a<<" Greater"<<endl;}
+    // else if ( b>=c){cout<<b<<" Gretaer"<<endl;}
+    // else { cout<<c<<" Greater"<<endl;}
+    // //ternary operator
+    // bool isAdult= (a>=18)?true:false;
+    
+}
