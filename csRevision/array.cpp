18ed970437384ef52cdc637f9b0d596f0682f9f7
Mohitku-Panigrahi <mohitkumarpanigrahi533@gmail.com>
2026-09-22 11:07:04 +0530

    Practice5

--- csRevision/array.cpp
+++ csRevision/array.cpp
@@ -6,14 +6,14 @@
 #include<iostream>
 using namespace std;
 void traversal(int arr[],int n){
-    for(int i=0;i <=n;i++)
+    for(int i=0;i <n;i++)
     {
         cout<<arr[i]<<" ";
     }
 }
 int main() {
     int arr[5]={1,2,3,4,5};
-    int n=5;
+    int n=size(arr);
     // for(int i=0;i <=n;i++)
     // {
     //     cout<<arr[i]<<" ";
